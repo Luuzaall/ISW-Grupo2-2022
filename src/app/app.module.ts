6e5ba01a37425b0af15bbf49a76393a979c53432
ignacioccugura <ignacioccugura@gmail.com>
2022-08-27 13:14:45 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatosPedidoComponent } from './datos-pedido/datos-pedido.component';
+import { FormaPagoComponent } from './forma-pago/forma-pago.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatosPedidoComponent
+    DatosPedidoComponent,
+    FormaPagoComponent
   ],
   imports: [
     BrowserModule,
