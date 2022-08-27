6177f6e311b4977e0ee0cf0502159a1e66d7d7e7
sabrina.passero <passero.sabrina@gmail.com>
2022-08-27 13:13:38 -0300

    se agrega carpeta direcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatosPedidoComponent } from './datos-pedido/datos-pedido.component';
+import { DireccionesComponent } from './components/direcciones/direcciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatosPedidoComponent
+    DatosPedidoComponent,
+    DireccionesComponent
   ],
   imports: [
     BrowserModule,
