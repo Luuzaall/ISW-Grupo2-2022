667a935d78a6740b608b5dece17e4d728d5c6410
sabrina.passero <passero.sabrina@gmail.com>
2022-08-27 16:20:33 -0300

    falta el mapa

--- src/app/components/direcciones/direcciones.component.ts
+++ src/app/components/direcciones/direcciones.component.ts
@@ -12,6 +12,15 @@ export class DireccionesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  AbrirMapaComercio(){
+    let img = new Image();
+    img.src="E:\ISW-Proyecto\ISW-Grupo2-2022\src\assets\imagenes\google-maps.svg";
+    
+  }
+  AbrirMapaCasa(){
+    window.open();
   }
 
 }
