282be36dad8db0e9dfaabb7c87a09d3d1d4b49af
Gaston Mastrapasqua <gastimastra@gmail.com>
2022-08-27 13:00:38 -0300

    (feat) creado componente datos producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatosPedidoComponent } from './datos-pedido/datos-pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatosPedidoComponent
   ],
   imports: [
     BrowserModule,
