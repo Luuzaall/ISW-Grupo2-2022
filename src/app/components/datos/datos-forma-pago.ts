f9f1b65c9a76161de723e8c3a735242c20b2ff58
Maru <83410136+Luuzaall@users.noreply.github.com>
2022-08-28 16:12:18 -0300

    Vista

---
+++ src/app/components/datos/datos-forma-pago.ts
@@ -0,0 +1,6 @@
+import { FormaPago } from '../../models/forma-pago';
+
+export const FormasPagos: FormaPago[] = [
+    { Nombre: "Efectivo"},
+    { Nombre: "Tarjeta Crédito / Débito"}
+];
--- src/app/components/forma-pago/forma-pago.component.ts
+++ src/app/components/forma-pago/forma-pago.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+
+import { FormaPago } from '../../models/forma-pago';
+
+// DATOS
+import { FormasPagos } from '../datos/datos-forma-pago'
 
 @Component({
   selector: 'app-forma-pago',
@@ -10,23 +15,35 @@ export class FormaPagoComponent implements OnInit {
   
   formaPago: number;
   efectivo: boolean;
+  FormasPagos: FormaPago[];
+  FormFormaPago: FormGroup;
+  FormFormaPagoEfectivo: FormGroup;
+  FormFormaPagoTarjeta: FormGroup;
 
-
-  FormFormaPago: FormGroup = new FormGroup({
-
-  });
+  submitted = false;
  
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    public formBuilder: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.FormasPagos = FormasPagos
+    this.crearControladorFormulario()
+
     this.efectivo = false;
     this.formaPago = 0;
   }
 
-  crearFormulario(){
-    this.FormFormaPago = this.fb.group({
-    
-    })
+  crearControladorFormulario(){
+    this.FormFormaPago = this.formBuilder.group({
+      FormaPago: [null, [Validators.required]],
+      MontoPagar: [null, [
+        Validators.required,
+        Validators.pattern('[1-9][0-9]{1,7}')
+      ]]
+        })
+    this.FormFormaPagoEfectivo = this.formBuilder.group({})    
+    this.FormFormaPagoTarjeta = this.formBuilder.group({})    
   }
 
   mostrandoEfectivo(resp: boolean): void{
