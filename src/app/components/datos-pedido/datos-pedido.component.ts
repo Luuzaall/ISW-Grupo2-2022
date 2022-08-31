a41772b20ea6464e07f2de6bb416d52608603db0
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2022-08-31 20:50:21 -0300

    Boton Continuar
    Agrego botón Continuar, y dejo algunas ideas generales comentadas

--- src/app/components/datos-pedido/datos-pedido.component.ts
+++ src/app/components/datos-pedido/datos-pedido.component.ts
@@ -1,6 +1,6 @@
 import { DatosPedido } from './../../models/datos-pedido';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'datos-pedido',
@@ -12,16 +12,29 @@ export class DatosPedidoComponent implements OnInit {
   mostrandoFechaYHora: boolean;
   hoy: Date;
   formDatosPedido = new FormGroup({
-    descripcionPedido: new FormControl(),
+    descripcionPedido: new FormControl('', [
+      Validators.required,
+    ]),
     cuandoRecibe: new FormControl(),
     foto: new FormControl(),
-    fechaYHora: new FormControl()
+    fechaYHora: new FormControl(),
+    //fecha: new FormControl('', [
+      //Validators.required,
+      //Validators.pattern(
+        //'(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+      //),
+    //]),
+    //hora: new FormControl(),
+    
   })
+
+  submitted = false;
   constructor() { }
 
   ngOnInit(): void {
     this.mostrandoFechaYHora = false;
     this.hoy = new Date();
+    
   }
 
   mostrarFechaYHora(valor: boolean){
@@ -32,5 +45,10 @@ export class DatosPedidoComponent implements OnInit {
       })
     }
       
+  }
+
+  continuar() : void{
+    this.submitted = true;
+    console.log("Acá se debería pasar a la parte de dirección retiro y envío..")
   }
 }
