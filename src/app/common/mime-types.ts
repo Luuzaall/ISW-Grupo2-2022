186369e14a1303e24e43491ad5ed6ccaa0ba1f57
Gaston Mastrapasqua <gastimastra@gmail.com>
2022-09-07 22:42:55 -0300

    validado tipo de archivo, tamaño y hora ingresada

---
+++ src/app/common/mime-types.ts
@@ -0,0 +1,5 @@
+export const MimeTypes = [
+    "image/jpeg",
+]
+
+    
--- src/app/components/datos-pedido/datos-pedido.component.ts
+++ src/app/components/datos-pedido/datos-pedido.component.ts
@@ -1,6 +1,7 @@
 import { DatosPedido } from './../../models/datos-pedido';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MimeTypes } from 'src/app/common/mime-types';
 
 @Component({
   selector: 'datos-pedido',
@@ -9,11 +10,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class DatosPedidoComponent implements OnInit {
   datosPedido: DatosPedido;
+  fechaMenorActual: boolean = false;
   mostrandoFechaYHora: boolean;
+  tamanioMaxFoto: number = 5242880; //5MB
   @Output() onContinuar = new EventEmitter();
   tzoffset: number; //offset in milliseconds
+  validFileType = MimeTypes;
   dateLocalISOString: string;
   archivos: File[] = [];
+  hoy: Date;
   formDatosPedido = new FormGroup({
     descripcionPedido: new FormControl('', [
       Validators.required,
@@ -36,31 +41,42 @@ export class DatosPedidoComponent implements OnInit {
     this.mostrandoFechaYHora = false;
     this.tzoffset = (new Date()).getTimezoneOffset() * 60000;
     this.dateLocalISOString = new Date(Date.now() - this.tzoffset).toISOString().slice(0,new Date().toISOString().lastIndexOf(":"));
-
+    this.hoy = new Date();
   }
 
   mostrarFechaYHora(valor: boolean){
+    this.fechaMenorActual = false;
     this.mostrandoFechaYHora = valor;
     // if (!this.mostrandoFechaYHora){
     //   this.formDatosPedido.value.fechaYHora = this.dateLocalISOString;
     // }
-      
   }
 
   continuar() : void{
     this.submitted = true;
-    if(this.formDatosPedido.invalid){
+    if(this.formDatosPedido.invalid || this.fechaMenorActual){
       return;
     }
     this.onContinuar.emit();
   }
 
   async addFile(target: any){
-    this.formDatosPedido.value.foto = target.files;
+    const archivoIngresado: File = target.files[0] as File;
+    if (!this.validFileType.includes(archivoIngresado.type))
+    alert('No se puede ingresar un archivo que no tenga el formato jpg');
+    else if (archivoIngresado.size > this.tamanioMaxFoto)
+    alert('El tamaño de la foto no puede superar los ' + (this.tamanioMaxFoto / 1048576) + 'MB.')
+    else
+    this.formDatosPedido.value.foto = target.files[0] as File;
   }
+  
+    validarFecha(target: any){
+      const fechaIngresada: Date = new Date(target.value);
+      if (fechaIngresada < this.hoy)
+        this.fechaMenorActual = true;
+      else
+        this.fechaMenorActual = false;
+}
 
-  validarFecha():boolean{
-    var fechaIngresada = document.getElementById("fechaHoraRecibo")?.nodeValue;
-    return true;
-  }
+
 }
