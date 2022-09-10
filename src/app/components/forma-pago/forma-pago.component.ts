5da287e88be22aa5076d7b984983292ebd64d56e
Maru <luuzaall@gmail.com>
2022-09-10 17:16:36 -0300

    Cambiado a lowercase variables

--- src/app/components/forma-pago/forma-pago.component.ts
+++ src/app/components/forma-pago/forma-pago.component.ts
@@ -10,7 +10,7 @@ import { FormaPago } from '../../models/forma-pago';
 import { Pedido } from '../../models/pedido';
 
 // DATOS
-import { FormasPagos } from '../datos/datos-forma-pago'
+import { formasPagos } from '../datos/datos-forma-pago'
 
 @Component({
   selector: 'app-forma-pago',
@@ -21,7 +21,7 @@ export class FormaPagoComponent implements OnInit {
   @Output() onContinuar = new EventEmitter();
   @Output() onVolver = new EventEmitter();
   //datos precargados
-  FormasPagos: FormaPago[];
+  formasPagos: FormaPago[];
 
   //datos pedido
   pedido: Pedido;
@@ -30,9 +30,9 @@ export class FormaPagoComponent implements OnInit {
   fecha: Date;
   
   //Formularios
-  FormFormaPago: FormGroup;
-  FormFormaPagoEfectivo: FormGroup;
-  FormFormaPagoTarjeta: FormGroup;
+  formFormaPago: FormGroup;
+  formFormaPagoEfectivo: FormGroup;
+  formFormaPagoTarjeta: FormGroup;
   
   //Para ver si el usuario ya apretó el "Enviar"
   // Es decir, cuando apretó enviar, recién ahí verfica los datos incompletos.
@@ -47,7 +47,7 @@ export class FormaPagoComponent implements OnInit {
     }
 
   ngOnInit(): void {
-    this.FormasPagos = FormasPagos
+    this.formasPagos = formasPagos
     this.fecha = new Date();
     this.efectivo = false;
     this.monto = this.datosDireccion.costoEnvio;
@@ -55,18 +55,18 @@ export class FormaPagoComponent implements OnInit {
   }
 
   crearControladorFormulario(){
-    this.FormFormaPago = this.formBuilder.group({
-      FormaPago: [null, [Validators.required]]
+    this.formFormaPago = this.formBuilder.group({
+      formaPago: [null, [Validators.required]]
       })
-    this.FormFormaPagoEfectivo = this.formBuilder.group({
-      MontoPagar: [null, [
+    this.formFormaPagoEfectivo = this.formBuilder.group({
+      montoPagar: [null, [
         Validators.required,
         Validators.pattern('[0-9][0-9]{0,7}'),
         Validators.min(this.monto)
       ]],
     })    
 
-    this.FormFormaPagoTarjeta = this.formBuilder.group({
+    this.formFormaPagoTarjeta = this.formBuilder.group({
       numeroTarjeta: [null, [
         Validators.required,
         Validators.pattern(('5[1-5][0-9]{14}')),
@@ -77,11 +77,11 @@ export class FormaPagoComponent implements OnInit {
       nombreYApellidoTitular: [null, 
         Validators.required],
 
-      FechaVencimientoMes:[null, [
+      fechaVencimientoMes:[null, [
         Validators.required,
         Validators.pattern(('^(([1-9])|(1[012]))$'))]],
       
-      FechaVencimientoAnio:[null, [
+      fechaVencimientoAnio:[null, [
         Validators.required,
         Validators.pattern('([2][0][2][2-9])|([2][0][3-9][0-9])'),
       ]],
@@ -106,8 +106,8 @@ export class FormaPagoComponent implements OnInit {
     let mesActual = fechaYHoraActual.getMonth()
     let anioActual = fechaYHoraActual.getFullYear()
 
-    let anioIngresado = this.FormFormaPagoTarjeta.get('FechaVencimientoAnio')?.value 
-    let mesIngresado = this.FormFormaPagoTarjeta.get('FechaVencimientoMes')?.value 
+    let anioIngresado = this.formFormaPagoTarjeta.get('fechaVencimientoAnio')?.value 
+    let mesIngresado = this.formFormaPagoTarjeta.get('fechaVencimientoMes')?.value 
 
     if (mesIngresado <= mesActual && anioActual == anioIngresado) {
       this.fechaVencimientoNOValida = true;
@@ -124,7 +124,7 @@ export class FormaPagoComponent implements OnInit {
     let datosTarjetaNuevo: DatosTarjeta = null as any
 
     if (this.formaPagoSeleccionado.sosEfectivo()){
-      montoAPagar = this.FormFormaPagoEfectivo.get('MontoPagar')?.value 
+      montoAPagar = this.formFormaPagoEfectivo.get('montoPagar')?.value 
     }
     else {
       let datosTarjeta = this.obtenerDatosTarjeta();
@@ -138,11 +138,11 @@ export class FormaPagoComponent implements OnInit {
   }
 
   obtenerDatosTarjeta(): any{
-    let numeroTarjeta = this.FormFormaPagoTarjeta.get('numeroTarjeta')?.value 
-    let nombreYApe = this.FormFormaPagoTarjeta.get('nombreYApellidoTitular')?.value 
-    let fechaVencimientoMes =  this.FormFormaPagoTarjeta.get('FechaVencimientoMes')?.value
-    let fechaVencimientoAnio =  this.FormFormaPagoTarjeta.get('FechaVencimientoAnio')?.value
-    let cvc = this.FormFormaPagoTarjeta.get('cvc')?.value
+    let numeroTarjeta = this.formFormaPagoTarjeta.get('numeroTarjeta')?.value 
+    let nombreYApe = this.formFormaPagoTarjeta.get('nombreYApellidoTitular')?.value 
+    let fechaVencimientoMes =  this.formFormaPagoTarjeta.get('fechaVencimientoMes')?.value
+    let fechaVencimientoAnio =  this.formFormaPagoTarjeta.get('fechaVencimientoAnio')?.value
+    let cvc = this.formFormaPagoTarjeta.get('cvc')?.value
 
     return [numeroTarjeta, nombreYApe, fechaVencimientoMes, fechaVencimientoAnio, cvc]
   }
